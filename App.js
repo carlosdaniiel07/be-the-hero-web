a093299dd6f953211671c7a5a5cfc264a06f2c20
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-05-10 17:57:48 -0300

    Criado estrutura de pastas

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import Routes from './src/routes';
 
-export default () => {
-  return (
-    <View style={style.container}>
-      <Text style={style.text}>Hello React Native</Text>
-    </View>
-  );
-};
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#FFF',
-  },
-});
+export default function App() {
+  return <Routes />;
+}
